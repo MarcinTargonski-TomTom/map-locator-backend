1d18feb4dcf35a9270d82a9a59e157ff72c79364
MarcinTargonski-TomTom <marcin.targonski@tomtom.com>
2025-07-24 15:57:50 +0200

    Spring initializr

---
+++ src/main/java/com/tomtom/locator/map/map_locator/MapLocatorApplication.java
@@ -0,0 +1,13 @@
+package com.tomtom.locator.map.map_locator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MapLocatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MapLocatorApplication.class, args);
+	}
+
+}
