9985c0293ec552ade031ea3d211cd98cb5986118
MarcinTargonski-TomTom <marcin.targonski@tomtom.com>
2025-07-28 09:29:50 +0200

    GetOverlapingRegion

--- src/main/java/com/tomtom/locator/map/map_locator/services/matcher/PlaceMatcherServiceImpl.java
+++ src/main/java/com/tomtom/locator/map/map_locator/services/matcher/PlaceMatcherServiceImpl.java
@@ -1,10 +1,16 @@
 package com.tomtom.locator.map.map_locator.services.matcher;
 
 import com.tomtom.locator.map.map_locator.model.CalculatedRoute;
+import com.tomtom.locator.map.map_locator.model.Point;
 import com.tomtom.locator.map.map_locator.model.PointOfInterest;
 import com.tomtom.locator.map.map_locator.model.Region;
 import com.tomtom.locator.map.map_locator.services.map.MapService;
 import lombok.RequiredArgsConstructor;
+import org.locationtech.jts.geom.Coordinate;
+import org.locationtech.jts.geom.Geometry;
+import org.locationtech.jts.geom.GeometryFactory;
+import org.locationtech.jts.geom.LinearRing;
+import org.locationtech.jts.geom.Polygon;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -22,10 +28,38 @@ public class PlaceMatcherServiceImpl implements PlaceMatcherService {
         return getOverlapingRegion(calculatedRoutes);
     }
 
-
     private Region getOverlapingRegion(List<CalculatedRoute> calculatedRoutes) {
-        throw new UnsupportedOperationException("Not implemented yet");
+        Geometry actualPolygon = convertRegionToJTSPolygon(calculatedRoutes.getFirst().getReachableRange());
+        for (var calculatedRoute : calculatedRoutes) {
+            Polygon polygon = convertRegionToJTSPolygon(calculatedRoute.getReachableRange());
+            actualPolygon = polygon.union(actualPolygon);
+        }
+        return convertPolygonToRegion((Polygon) actualPolygon);
     }
 
 
+    private Polygon convertRegionToJTSPolygon(Region region) {
+        GeometryFactory geometryFactory = new GeometryFactory();
+
+        Coordinate[] coordinates = region.getBoundary().stream()
+                .map(point -> new Coordinate(point.getLongitude(), point.getLatitude()))
+                .toArray(Coordinate[]::new);
+
+        if (!coordinates[0].equals(coordinates[coordinates.length - 1])) {
+            coordinates = java.util.Arrays.copyOf(coordinates, coordinates.length + 1);
+            coordinates[coordinates.length - 1] = coordinates[0];
+        }
+
+        LinearRing shell = geometryFactory.createLinearRing(coordinates);
+        return geometryFactory.createPolygon(shell, null);
+    }
+
+    private Region convertPolygonToRegion(Polygon polygon) {
+        Region region = new Region(null, List.of());
+        Coordinate[] coordinates = polygon.getCoordinates();
+        for (var coordinate : coordinates) {
+            region.getBoundary().add(new Point(coordinate.x, coordinate.y));
+        }
+        return region;
+    }
 }
