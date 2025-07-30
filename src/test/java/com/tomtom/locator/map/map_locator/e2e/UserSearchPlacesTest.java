cbefa6640c1cb21643ef6c468c99987d6e9cc16b
Tomasz Grzelik <tomasz.grzelik@tomtom.com>
2025-07-30 16:23:01 +0200

    Added search by pois when authenticated in e2e.

--- src/test/java/com/tomtom/locator/map/map_locator/e2e/UserSearchPlacesTest.java
+++ src/test/java/com/tomtom/locator/map/map_locator/e2e/UserSearchPlacesTest.java
@@ -28,18 +28,7 @@ public class UserSearchPlacesTest extends BaseIntegrationTest {
         //expect to not allow to search when not authenticated
         given()
             .contentType(ContentType.JSON)
-            .body("""
-            {
-              "name": "Central Park",
-              "center": {
-                "latitude": 40.785091,
-                "longitude": -73.968285
-              },
-              "value": 100,
-              "budgetType": "PREMIUM",
-              "travelMode": "CAR"
-            }
-            """)
+            .body(EXAMPLE_POI_LIST_JSON)
         .when()
             .post("/locations/v1/matchLocation")
         .then()
@@ -77,8 +66,33 @@ public class UserSearchPlacesTest extends BaseIntegrationTest {
             .statusCode(HttpStatus.OK.value())
         .extract();
 
+        //and
         var authToken = response.body().jsonPath().getString("auth");
-
         Assertions.assertNotNull(authToken);
+
+        //expect to allow search when authenticated
+        given()
+            .contentType(ContentType.JSON)
+            .header("Authorization", "Bearer " + authToken)
+            .body(EXAMPLE_POI_LIST_JSON)
+        .when()
+            .post("/locations/v1/matchLocation")
+        .then()
+            .statusCode(HttpStatus.OK.value());
     }
+
+    private static final String EXAMPLE_POI_LIST_JSON = """
+            [
+                {
+                  "name": "Central Park",
+                  "center": {
+                    "latitude": 40.785091,
+                    "longitude": -73.968285
+                  },
+                  "value": 100,
+                  "budgetType": "DISTANCE",
+                  "travelMode": "CAR"
+                }
+            ]
+            """;
 }
