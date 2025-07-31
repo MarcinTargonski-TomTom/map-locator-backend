4b90b294da881280aac9ffbed9702b42a4790003
Tomasz Grzelik <73581787+TomaszGrzelik-TomTom@users.noreply.github.com>
2025-07-31 10:43:54 +0200

    Update src/test/java/com/tomtom/locator/map/map_locator/e2e/UserSearchPlacesTest.java
    Co-authored-by: BartekDygasinski-TomTom <bartlomiej.dygasinski@tomtom.com>

--- src/test/java/com/tomtom/locator/map/map_locator/e2e/UserSearchPlacesTest.java
+++ src/test/java/com/tomtom/locator/map/map_locator/e2e/UserSearchPlacesTest.java
@@ -12,7 +12,7 @@ import java.util.Map;
 
 import static io.restassured.RestAssured.*;
 
-public class UserSearchPlacesTest extends BaseIntegrationTest {
+public class UserSearchPlacesIT extends BaseE2ETest {
     @Test
     public void userShouldBeAbleToFindPlaces() {
         //expect to not allow to search when not authenticated
