000ff7822fef0418073e631dd39fdd2ccaf303d3
MarcinTargonski-TomTom <marcin.targonski@tomtom.com>
2025-08-05 09:36:24 +0200

    Liquibase config

---
+++ src/main/java/com/tomtom/locator/map/map_locator/config/LiquibaseRunner.java
@@ -0,0 +1,52 @@
+package com.tomtom.locator.map.map_locator.config;
+
+import liquibase.Contexts;
+import liquibase.LabelExpression;
+import liquibase.Liquibase;
+import liquibase.database.Database;
+import liquibase.database.DatabaseFactory;
+import liquibase.database.jvm.JdbcConnection;
+import liquibase.resource.ClassLoaderResourceAccessor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.context.event.ApplicationReadyEvent;
+import org.springframework.context.event.EventListener;
+import org.springframework.core.annotation.Order;
+import org.springframework.stereotype.Component;
+
+import javax.sql.DataSource;
+import java.sql.Connection;
+
+@Component
+public class LiquibaseRunner {
+
+    private static final Logger logger = LoggerFactory.getLogger(LiquibaseRunner.class);
+
+    @Autowired
+    private DataSource dataSource;
+
+    @EventListener
+    @Order(1000)
+    public void onApplicationReady(ApplicationReadyEvent event) {
+        logger.info("Running Liquibase after application startup...");
+
+        try (Connection connection = dataSource.getConnection()) {
+            Database database = DatabaseFactory.getInstance()
+                    .findCorrectDatabaseImplementation(new JdbcConnection(connection));
+
+            Liquibase liquibase = new Liquibase(
+                    "db/changelog/db.changelog-master.yaml",
+                    new ClassLoaderResourceAccessor(),
+                    database
+            );
+
+            liquibase.update(new Contexts(), new LabelExpression());
+            logger.info("Liquibase update completed successfully");
+
+        } catch (Exception e) {
+            logger.error("Error running Liquibase: ", e);
+            throw new RuntimeException("Failed to run Liquibase", e);
+        }
+    }
+}
